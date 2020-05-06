7f295b847f712b8446527e3bf5e4b8a7b204e21f
Natalia Chrząścik <56516256+nataliachrzascik@users.noreply.github.com>
2020-05-06 13:23:16 +0200

    Add files via upload

---
+++ precache-manifest.43cd315c8bca1e4d2a0ddc44650acae5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e17cb20653affe91913055bdf8d8449",
+    "url": "/weatherApp/index.html"
+  },
+  {
+    "revision": "f287ce87d51c087e4468",
+    "url": "/weatherApp/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "07906c49ff5d686f3bb3",
+    "url": "/weatherApp/static/css/main.e1d70895.chunk.css"
+  },
+  {
+    "revision": "f287ce87d51c087e4468",
+    "url": "/weatherApp/static/js/2.29e0b242.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weatherApp/static/js/2.29e0b242.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07906c49ff5d686f3bb3",
+    "url": "/weatherApp/static/js/main.e5613097.chunk.js"
+  },
+  {
+    "revision": "140da1ebad7652361320",
+    "url": "/weatherApp/static/js/runtime-main.be8c1dcf.js"
+  },
+  {
+    "revision": "c863f8028c2505f92540e0ba7c379002",
+    "url": "/weatherApp/static/media/PermanentMarker-Regular.c863f802.ttf"
+  },
+  {
+    "revision": "6494ab5b3760b5e2cc46f5566a245371",
+    "url": "/weatherApp/static/media/stars.6494ab5b.png"
+  }
+]);
